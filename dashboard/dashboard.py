e4dfb1b6ab6a6cee346f9716c9b0b21c5911c6c2
Kritin P <pkritin@gmail.com>
2023-12-09 16:24:10 +0530

    final commit

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,7 +9,6 @@ def delete_files_in_folder(folder_path):
             file_path = os.path.join(folder_path, filename)
             if os.path.isfile(file_path):
                 os.remove(file_path)
-                # print(f"Deleted: {file_path}")
     except OSError as e:
         print(f"Error: {e}")
 
@@ -23,10 +22,8 @@ time.sleep(5)
     
 G = nx.Graph()
 
-# Set to store unique edges
 edges_set = set()
 
-# Read the edges from the file and add unique edges to the graph
 with open("/home/kritin/College/SDS/Project/dashboard/src/general/edges.txt", "r") as file:
     for line in file:
         edge = tuple(sorted(map(int, line.strip().split(','))))
@@ -34,15 +31,12 @@ with open("/home/kritin/College/SDS/Project/dashboard/src/general/edges.txt", "r
             edges_set.add(edge)
             G.add_edge(*edge)
 
-# Generate node information (dummy information for demonstration)
 node_info = {node: f"Node {node} Information" for node in G.nodes()}
 
-# Plotting the graph
 pos = nx.shell_layout(G, [[node for node in G.nodes()]])
 nx.draw(G, pos, with_labels=True, node_color='skyblue', node_size=1000, font_weight='bold')
 
 plt.savefig('/home/kritin/College/SDS/Project/dashboard/src/assets/graph.png')  # Save the graph as an image file
-# plt.show()  # Uncomment if you also want to display the graph
 
 npm_command = "npm run dev"
 directory_path = "/home/kritin/College/SDS/Project/dashboard"
